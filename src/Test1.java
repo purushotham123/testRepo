615bacdcb0ed1764645a92fda340261e273b9267
purushotham.thalari@ca.com <Pt#ri$123>
2014-05-15 01:06:04 +0530

    second commit test2

--- src/Test1.java
+++ src/Test1.java
@@ -1,6 +1,13 @@
 
 public class Test1 {
 public static void main(String[] args) {
-	System.out.println("Inside 1");
+	System.out.println("Inside 1 branch name Master");
+	System.out.println("Inside 1 branch name Master1");
+	System.out.println("Inside 1 branch name Master2");
+	
+	System.out.println("Inside 1 branch name Master3");
+	System.out.println("Inside 1 branch name Master4");
+	System.out.println("Inside 1 branch name Master5");
+	System.out.println("Inside 1 branch name Master6");
 }
 }
--- src/Test2.java
+++ src/Test2.java
@@ -5,7 +5,7 @@ public class Test2 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("inside Test2");
+		System.out.println("inside Test2 branch name Master");
 
 	}
 
